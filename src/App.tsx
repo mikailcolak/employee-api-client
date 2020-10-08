6705c6a74e15b1a740dc7ce9c5aa8e2bc29a4a82
Mikail ÇOLAK <mikailcolak@gmail.com>
2020-10-08 13:27:02 +0300

    App component refactored completely.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import './App.css';
 
 import { connect, ConnectedProps } from "react-redux";
-import { createCompanyAction, fetchCompaniesAction } from "./store/company/actions";
+import { fetchCompaniesAction } from "./store/company/actions";
 import { AppState } from "./store";
 import { Company, CompaniesState } from "./store/company/types";
-import { LoadingState } from "./store/common-types";
 import { fetchEmployeesAction } from "./store/employee/actions";
 import MainLayout from "./layouts/main";
 import { CompanyList } from "./components/company-list";
 import { EmployeeList } from "./components/employee-list";
+import { CompanyDetails } from "./components/company-details";
 import { EmployeesState } from "./store/employee/types";
+import { setCrudState } from "./store/crud/actions";
+import { HandlerType } from "./api";
 
 const mapStateToProps = (state: AppState): {
   companies: CompaniesState,
-  employees: EmployeesState
+  employees: EmployeesState,
 } => ({
   companies: state.content.companies,
   employees: state.content.employees,
@@ -23,53 +25,51 @@ const mapStateToProps = (state: AppState): {
 const mapDispatchToProps = {
   fetchCompanies: (page: number, itemsPerPage: number) => fetchCompaniesAction(page, itemsPerPage),
   fetchEmployees: (companyId: number, page: number, itemsPerPage: number) => fetchEmployeesAction(companyId, page, itemsPerPage),
-  createCompany: (company: Company) => createCompanyAction(company),
+  setCrudTarget: (handler: HandlerType, target: Company) => setCrudState<Company>(handler, "company", target),
 };
 
 const connector = connect(mapStateToProps, mapDispatchToProps);
 
 type Props = ConnectedProps<typeof connector>;
 
-function App({ createCompany, fetchCompanies, fetchEmployees, companies, employees }: Props) {
+class App extends React.Component<Props> {
 
-  var [selectedCompany, setSelectedCompany] = useState<Company | undefined | null>(undefined);
-
-  const _setSelectedCompany = (company: Company | undefined | null) => {
-    if (company && company.id != employees.companyId) {
-      fetchEmployees(company.id, employees.page, employees.itemsPerPage);
-    }
-    setSelectedCompany(company);
+  componentDidMount() {
+    this.props.fetchCompanies(this.props.companies.page || 0, this.props.companies.itemsPerPage || 0);
   }
 
-  useEffect(() => {
-    // fetchCompanies(companies.page || 0, companies.itemsPerPage || 0);
-  });
+  render() {
+    const {
+      employees,
+      companies,
+      setCrudTarget,
+    } = this.props;
 
-  return (
-    <MainLayout>
-      <div className="two-column">
-        <div className="left-column">
-          <CompanyList
-            companies={companies}
-            companyChanged={_setSelectedCompany}
-            selectedCompany={selectedCompany}
-            loadCompanies={fetchCompanies}
-            createCompany={createCompany}
-          />
+    return (
+      <MainLayout>
+        <div className="two-column">
+          <div className="left-column">
+            <CompanyList />
+          </div>
+          <div className="right-column">
+            {
+              companies.crud?.target &&
+              <>
+                <CompanyDetails
+                  setCrudTarget={setCrudTarget}
+                  company={companies.crud?.target}
+                  employees={employees}
+                />
+                <span className="hr-spacer" />
+                <EmployeeList />
+              </>
+            }
+          </div>
         </div>
-        <div className="right-column">
-          {
-            selectedCompany && employees.contentState == LoadingState.Loaded &&
-            <EmployeeList
-              company={selectedCompany}
-              employees={employees}
-              loadEmployees={(page: number, itemsPerPage: number) => fetchEmployees(selectedCompany?.id || 0, page, itemsPerPage)}
-            />
-          }
-        </div>
-      </div>
-    </MainLayout>
-  );
+      </MainLayout>
+    );
+  }
 }
+
 
 export default connector(App);
